c0b27810ea66084fce2a5ebf323255cb2941b47b
615374 <giselalanzillotta@gmail.com>
2022-07-18 23:38:45 -0300

    dom

---
+++ stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "Remera Trasher", tipo: "remera", cantidad: 1, desc: "Remera", precio: 3500, talle: "S", img: './img/remera.jpg'},
+    {id: 2, nombre: "Anorak Black", tipo: "campera", cantidad: 1, desc: "Buzo", precio: 4800, talle: "M", img: './img/campera.jpg'},
+    {id: 3, nombre: "Pantalon Cargo", tipo: "pantalon", cantidad: 1, desc: "Pantalon", precio: 7500, talle: "L", img: './img/pantalon.jpg'},
+    
+]
